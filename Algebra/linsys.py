dbafc454d72178a8a04dd1993e6e10f1bac144e9
jie <fgnna@qq.com>
2018-06-27 15:00:13 +0800

    完成RREF消除算法

--- Algebra/linsys.py
+++ Algebra/linsys.py
@@ -33,7 +33,7 @@ class LinearSystem(object):
 
     def multiply_coefficient_and_row(self, coefficient, row):
         p = self.planes[row]
-        self.planes[row] = Plane(p.normal_vector.times_scalar(coefficient), p.constant_term * coefficient)
+        return Plane(p.normal_vector.times_scalar(coefficient), p.constant_term * coefficient)
 
     '''
     某一行加法到另一行
@@ -45,9 +45,7 @@ class LinearSystem(object):
         if coefficient == 0:
             return
 
-        self.multiply_coefficient_and_row(coefficient, row_to_add)
-        print()
-        p_to_add = self.planes[row_to_add]
+        p_to_add = self.multiply_coefficient_and_row(coefficient, row_to_add)
         p_added_to = self.planes[row_to_be_added_to]
         p_new = Plane(Vector([x + y for x, y in zip(p_to_add.normal_vector, p_added_to.normal_vector)]),
                       p_to_add.constant_term + p_added_to.constant_term)
@@ -71,68 +69,115 @@ class LinearSystem(object):
         return indices
 
     def swap_with_row_below_for_nonzero_coefficient_if_able(self, row, col):
-        #平面数
+        # 平面数
         num_equations = len(self)
-        #从下一个平面开始计算交换
+        # 从下一个平面开始计算交换
         for k in range(row + 1, num_equations):
-            #直到找到一个指定维度不为0的平面，并进行交换后 返回
+            # 直到找到一个指定维度不为0的平面，并进行交换后 返回
             coefficient = MyDecimal(self[k].normal_vector[col])
             if not coefficient.is_near_zero():
                 self.swap_rows(row, k)
                 return True
         return False
 
-    def clear_coefficients_below(self, row, col):
-        #平面数
-        num_equations = len(self)
-        #要消除的系数
-        beta = MyDecimal(self[row].normal_vector[col])
-        #找出下面每行的同位系数，进行倍减消除
-        for k in range(row + 1, num_equations):
-            n = self[k].normal_vector
-            gamma = n[col]
-            #计算出倍数值，使负倍数并让其相加，得出消除系数的倍数
-            alpha = -gamma / beta
-            print(alpha)
-            print(self[k])
-            self.add_multiple_times_row_to_row(alpha, row, k)
+    def printLinsys(self):
+        if len(self) == 2:
+            print("{}\n{}".format(self[0], self[1]))
+        if len(self) == 3:
+            print("{}\n{}\n{}".format(self[0], self[1], self[2]))
+        if len(self) == 4:
+            print("{}\n{}\n{}\n{}".format(self[0], self[1], self[2], self[3]))
 
     def compute_triangular_form(self):
+        print("**************************************************")
         system = deepcopy(self)
 
-        def printLinsys():
-            if len(system) == 2:
-                print("第次冒泡：\n{}\n{}:".format(system[0], system[1]))
-            if len(system) == 3:
-                print("第次冒泡：\n{}\n{}\n{}:".format(system[0], system[1], system[2]))
-            if len(system) == 4:
-                print("第次冒泡：\n{}\n{}\n{}\n{}:".format(system[0], system[1], system[2], system[3]))
-
-        #维度
+        # 维度
         num_equations = len(system)
-        #平面数
+        # 平面数
         num_variables = system.dimension
         j = 0
+        print("消除前--------------------------------------")
+        system.printLinsys()
         for i in range(num_equations):
             while j < num_variables:
                 c = MyDecimal(system[i].normal_vector[j])
                 if c.is_near_zero():
-                    #这一维度为零，进行交换环节
+                    # 这一维度为零，进行交换环节
                     swap_succeeded = system.swap_with_row_below_for_nonzero_coefficient_if_able(i, j)
-                    #如何没有可进行交换行，维度位置右移一位，继续进行交换环节
+                    # 如何没有可进行交换行，维度位置右移一位，继续进行交换环节
                     if not swap_succeeded:
                         j += 1
                         continue
-                #当前平面交换完毕后，进行系数消除
-                print("sssss--------------------------------------")
-                printLinsys()
+                # 当前平面交换完毕后，进行系数消除
                 system.clear_coefficients_below(i, j)
-                print("+++++--------------------------------------")
-                printLinsys()
                 j += 1
                 break
-        printLinsys()
+        print("消除后--------------------------------------")
+        system.printLinsys()
+        print("消除完毕--------------------------------------")
         return system
+
+    '''
+    进行系数消除得出最终的三角化方程
+    '''
+
+    def clear_coefficients_below(self, row, col):
+        # 平面数
+        num_equations = len(self)
+        # 要消除的系数
+        beta = MyDecimal(self[row].normal_vector[col])
+        # 找出下面每行的同位系数，进行倍减消除
+        for k in range(row + 1, num_equations):
+            n = self[k].normal_vector
+            gamma = n[col]
+            # 计算出倍数值，使负倍数并让其相加，得出消除系数的倍数
+            alpha = -gamma / beta
+            self.add_multiple_times_row_to_row(alpha, row, k)
+
+    '''
+    将三角化后的方程进行RREF计算
+    '''
+
+    def compute_rref(self):
+        system = self.compute_triangular_form()
+        print("RREF消除開始--------------------------------------")
+        # 维度
+        num_equations = len(system)
+        # 點數
+        num_variables = system.dimension
+
+        # 思路就是做一次反向消除，即从底部往上消除,因为前一步已经进行了三角化
+        for i in range(num_equations - 1, 0, -1):
+            j = 0
+            while j < num_variables:
+                c = MyDecimal(system[i].normal_vector[j])
+                if c.is_near_zero():
+                    j += 1
+                    continue
+                system.clear_coefficients_after(i, j)
+                num_equations += 1
+                break
+
+        system.printLinsys()
+        print("RREF消除完毕--------------------------------------")
+        return system
+
+    '''
+    进行反向系数消除
+    '''
+    def clear_coefficients_after(self, row, col):
+        # 要消除的系数
+        beta = MyDecimal(self[row].normal_vector[col])
+        # 找出下面每行的同位系数，进行倍减消除
+        for k in range(row - 1, -1, -1):
+            gamma = self[k].normal_vector[col]
+            if gamma == 0:
+                continue
+
+            # 计算出倍数值，使负倍数并让其相加，得出消除系数的倍数
+            alpha = -gamma / beta
+            self.add_multiple_times_row_to_row(alpha, row, k)
 
     def __len__(self):
         return len(self.planes)
@@ -247,17 +292,17 @@ class MyDecimal(Decimal):
 #         t[1] == Plane(constant_term='1')):
 #     print('test case 2 failed')
 
-p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['0', '1', '0']), constant_term='2')
-p3 = Plane(normal_vector=Vector(['1', '1', '-1']), constant_term='3')
-p4 = Plane(normal_vector=Vector(['1', '0', '-2']), constant_term='2')
-s = LinearSystem([p1, p2, p3, p4])
-t = s.compute_triangular_form()
-if not (t[0] == p1 and
-        t[1] == p2 and
-        t[2] == Plane(normal_vector=Vector(['0', '0', '-2']), constant_term='2') and
-        t[3] == Plane()):
-    print('test case 3 failed')
+# p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
+# p2 = Plane(normal_vector=Vector(['0', '1', '0']), constant_term='2')
+# p3 = Plane(normal_vector=Vector(['1', '1', '-1']), constant_term='3')
+# p4 = Plane(normal_vector=Vector(['1', '0', '-2']), constant_term='2')
+# s = LinearSystem([p1, p2, p3, p4])
+# t = s.compute_triangular_form()
+# if not (t[0] == p1 and
+#         t[1] == p2 and
+#         t[2] == Plane(normal_vector=Vector(['0', '0', '-2']), constant_term='2') and
+#         t[3] == Plane()):
+#     print('test case 3 failed')
 
 # p1 = Plane(normal_vector=Vector(['0', '1', '1']), constant_term='1')
 # p2 = Plane(normal_vector=Vector(['1', '-1', '1']), constant_term='2')
@@ -268,3 +313,43 @@ if not (t[0] == p1 and
 #         t[1] == Plane(normal_vector=Vector(['0', '1', '1']), constant_term='1') and
 #         t[2] == Plane(normal_vector=Vector(['0', '0', '-9']), constant_term='-2')):
 #     print('test case 4 failed')
+
+''' reff 测试用例 '''
+
+p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0', '1', '1']), constant_term='2')
+s = LinearSystem([p1, p2])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term='-1') and
+        r[1] == p2):
+    print('test case 1 failed')
+
+p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='2')
+s = LinearSystem([p1, p2])
+r = s.compute_rref()
+if not (r[0] == p1 and
+        r[1] == Plane(constant_term='1')):
+    print('test case 2 failed')
+
+p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0', '1', '0']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['1', '1', '-1']), constant_term='3')
+p4 = Plane(normal_vector=Vector(['1', '0', '-2']), constant_term='2')
+s = LinearSystem([p1, p2, p3, p4])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term='0') and
+        r[1] == p2 and
+        r[2] == Plane(normal_vector=Vector(['0', '0', '-2']), constant_term='2') and
+        r[3] == Plane()):
+    print('test case 3 failed')
+
+p1 = Plane(normal_vector=Vector(['0', '1', '1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['1', '-1', '1']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['1', '2', '-5']), constant_term='3')
+s = LinearSystem([p1, p2, p3])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term=Decimal('23') / Decimal('9')) and
+        r[1] == Plane(normal_vector=Vector(['0', '1', '0']), constant_term=Decimal('7') / Decimal('9')) and
+        r[2] == Plane(normal_vector=Vector(['0', '0', '1']), constant_term=Decimal('2') / Decimal('9'))):
+    print('test case 4 failed')
--- Algebra/testcode.py
+++ Algebra/testcode.py
@@ -7,7 +7,7 @@ print('{:>14}'.format('陈某某'))
 print('{:<14}'.format('陈某某'))
 print('{:*<14}'.format('陈某某'))  # 用14个字符的宽度，左对齐，其他字符用*填充
 print('{:&>14}'.format('陈某某'))  # 填充和对齐^<>分别表示居中、左对齐、右对齐，后面带宽度
-for i in range( 5):
+for i in range( 4,-1,-1):
     print(i)
 
 p = 2
